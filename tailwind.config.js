3f4a5b58c93a37769443d1a20a7ec0ba289f1d10
WSishara96 <surangaishara8@gmail.com>
2022-12-12 07:50:57 +0530

    (folder structure and basic pages)

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        'cr-black': '#1e1e27',
+        'cr-white': '#fffffe',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
