41a9ab1e6cf27001c05fc95c8a727bda397b0c9b
WSishara96 <surangaishara8@gmail.com>
2022-12-12 09:33:41 +0530

    (folder structure and basic pages)

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import MaintainMode from '../components/MaintainMode';
 
 const contact = () => (
   <div>
-    <MaintainMode />
+    <h1 className="text-4xl font-bold text-center my-10">About Us</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque corporis delectus earum esse facilis, ipsum, magni maxime minima molestias natus nesciunt odio optio perferendis porro reprehenderit, rerum sunt totam voluptate.</p>
   </div>
 );
 
