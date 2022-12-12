7aeec1ec47cd401b240ea2861238d8e93d4c4168
WSishara96 <surangaishara8@gmail.com>
2022-12-12 09:38:03 +0530

    (folder structure and basic pages)

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const about = () => (
-  <div>
+  <div className='min-h-[60vh]'>
     <h1 className="text-4xl font-bold text-center my-10">About Us</h1>
 
-    <p className="text-justify px-10 md:px-20 h-80">
+    <p className="text-justify px-10 md:px-20">
       At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti
       atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident,
       similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum
